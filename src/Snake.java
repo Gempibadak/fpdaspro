9f00028b02649baf634335aa3967f6592a0f9da3
Gempibadak <156584175+Gempibadak@users.noreply.github.com>
2024-06-28 22:47:08 +0700

    Update Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -5,11 +5,11 @@
  * Group Capstone Project: Snake and Ladder Game
  * -----------------------------------------------------
  * Class    : C
- * Group    : XX
+ * Group    : 6
  * Members  :
- * 1. Student ID - Full Name
- * 2. Student ID - Full Name
- * 3. Student ID - Full Name
+ * 1. 5026231033 - Ayu Alfia Putri
+ * 2. 5026231143 - Muhammad Ridho Utomo
+ * 3. 5026231208 - Alfan Ghofari Prasanna Firmansyah
  * ------------------------------------------------------
  */
 
